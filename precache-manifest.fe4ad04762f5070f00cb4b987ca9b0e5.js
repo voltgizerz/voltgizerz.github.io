ae38363bb671da8ef5bdd562dc756c71a307af72
Felix Fernando <54938467+voltgizerz@users.noreply.github.com>
2020-07-12 01:22:02 +0700

    Updates

---
+++ precache-manifest.fe4ad04762f5070f00cb4b987ca9b0e5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9adb46019583eb6dd64bd8f762fd97e1",
+    "url": "/voltgizerz.github.io/index.html"
+  },
+  {
+    "revision": "1a0fbbd62a519ad0f7b4",
+    "url": "/voltgizerz.github.io/static/css/main.d23f9410.chunk.css"
+  },
+  {
+    "revision": "d006efdc4ac50a87fd1a",
+    "url": "/voltgizerz.github.io/static/js/2.1f64849d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/voltgizerz.github.io/static/js/2.1f64849d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a0fbbd62a519ad0f7b4",
+    "url": "/voltgizerz.github.io/static/js/main.f3729543.chunk.js"
+  },
+  {
+    "revision": "0dedcfe22a8486bf3ea8",
+    "url": "/voltgizerz.github.io/static/js/runtime-main.bfd1c6a2.js"
+  },
+  {
+    "revision": "3e54ed15b9cd877c5223f5ecf64579df",
+    "url": "/voltgizerz.github.io/static/media/github.3e54ed15.png"
+  },
+  {
+    "revision": "8ac3b5d4477a35d7c4e9f75fc077358d",
+    "url": "/voltgizerz.github.io/static/media/mail.8ac3b5d4.png"
+  },
+  {
+    "revision": "53884c83398356a92fed61355e7bfd6c",
+    "url": "/voltgizerz.github.io/static/media/profile.53884c83.png"
+  }
+]);
