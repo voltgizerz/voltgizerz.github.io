5310c333b966febfc7d9a00dd350d9bf5132f547
voltgizerz <felix.asui@gmail.com>
2022-01-20 21:49:51 +0700

    Update

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import profile from '../Components/public/image/profile.png';
 import linkedin from '../Components/public/image/linkedin.png'
 import twitter from '../Components/public/image/twitter.png'
 import github from '../Components/public/image/github.png'
@@ -10,11 +9,11 @@ class Banner extends Component {
     render() {
         return (
             <section className="container-banner">
-                <img id="profilepic" src={profile} width="170" height="170" alt="profilepic" />
+                <img id="profilepic" src="https://avatars.githubusercontent.com/u/54938467?s=400&u=224f8c7b42b3da22a074dc0124959773796fa9e8&v=4" width="170" height="170" alt="profilepic" />
                 <h1>Felix Fernando Wijaya</h1>
                 <h4>Software Engineer</h4>
-                <h4><i>"A software engineer, I am majoring in Informatics at Atma Jaya Yogyakarta University with an interest in software development, especially in back-end processes. I love to create something impactful and learn about technology."</i></h4>
-                <h5>Skills: Go, Python, Node.js, Ruby on Rails, Node.js, MySQL, MongoDB, GraphQL, REST APIs, Microservice, Docker.</h5>
+                <h4><i>"A software engineer, I am major in Informatics at Atma Jaya Yogyakarta University with an interest in software development, especially in back-end processes. I love to create impactful software and learn about technology."</i></h4>
+                <h5>Skills: Go, Python, Java, Node.js, Ruby on Rails, Node.js, MySQL, MongoDB, GraphQL, REST APIs, Git, Microservice, Docker.</h5>
                 <a href="https://www.linkedin.com/in/felix-fernando-wijaya/"><img src={CV} width="40" height="40" alt="LinkedIn" /></a>
                 <section id="skillheader" className="flex-project-container">
                     <div><a href="https://www.linkedin.com/in/felix-fernando-wijaya/" target="_blank"
