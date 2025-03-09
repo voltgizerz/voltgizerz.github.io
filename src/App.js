736a54ca251eca2b972069d84c32db1c63d3a532
voltgizerz <felix.asui@gmail.com>
2025-03-09 21:50:38 +0700

    Update analytics

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useEffect } from "react";
+import ReactGA from "react-ga4";
 import './App.css';
-import Header from './Components/Header'
-import Navbar from './Components/Navbar'
-import Banner from './Components/Banner'
-import Footer from './Components/Footer'
+import Header from './Components/Header';
+import Navbar from './Components/Navbar';
+import Banner from './Components/Banner';
+import Footer from './Components/Footer';
+
+
 function App() {
+  useEffect(() => {
+    ReactGA.initialize("G-G50YXWSMCL");
+    ReactGA.send("pageview");
+  }, []);
+
   return (
     <div className="App">
   
